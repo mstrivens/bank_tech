f301f0b3e40ca9c17ae186a9d46ab677e566c680
mstrivens <max.strivens@live.co.uk>
2021-05-05 12:55:48 +0100

    Account initializes with 0 balance on account creation

--- lib/account.rb
+++ lib/account.rb
@@ -2,6 +2,7 @@ class Account
   attr_reader :balance
 
   def initialize
+    @balance = 0
   end
 
   def credit(amount)
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,11 @@
 require 'account'
 
 describe Account do
+  describe '#balance' do
+    it 'initializes on zero' do
+      expect(subject.balance).to eq 0
+    end
+  end
   describe '#credit' do
     context 'when an account is creditted it creates a transaction object with 1 credit' do
       it 'adds 1 to the balance' do
