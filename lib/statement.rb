bf4a1d4b32f6edea943b8098f4cdeb0668c51d27
mstrivens <max.strivens@live.co.uk>
2021-05-05 16:17:17 +0100

    refactored to move methods into private methods

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,14 +1,18 @@
 class Statement
   def print_statement(transactions)
     print_header
-    transactions.each {|element|
-    print "#{element.date} || #{element.credit} || #{element.debit} || #{element.balance}"
-  }
+    print_transactions(transactions)
   end
 
   private
 
   def print_header
     puts ["Date || Credit || Debit || Balance"]
+  end
+
+  def print_transactions(transactions)
+    transactions.each {|element|
+    print "#{element.date} || #{element.credit} || #{element.debit} || #{element.balance}"
+  }
   end
 end
